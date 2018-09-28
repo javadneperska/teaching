152d921c503192789feb7c33a426afd27adeb715
Jozef Salanci <jozef.salanci@globallogic.com>
2018-09-28 14:51:04 +0200

    cal1

--- domace_ulohy/3/jozef.salanci/src/main/java/Calculator.java
+++ domace_ulohy/3/jozef.salanci/src/main/java/Calculator.java
@@ -40,6 +40,91 @@ public class Calculator {
     private static String outputSK = "Výsledky";
     private static String menuQuitSK = "Koniec";
 
+    public enum Color {
+        RESET("\033[0m"),
+
+        // Regular Colors
+        BLACK("\033[0;30m"),    // BLACK
+        RED("\033[0;31m"),      // RED
+        GREEN("\033[0;32m"),    // GREEN
+        YELLOW("\033[0;33m"),   // YELLOW
+        BLUE("\033[0;34m"),     // BLUE
+        MAGENTA("\033[0;35m"),  // MAGENTA
+        CYAN("\033[0;36m"),     // CYAN
+        WHITE("\033[0;37m"),    // WHITE
+
+        // Bold
+        BLACK_BOLD("\033[1;30m"),   // BLACK
+        RED_BOLD("\033[1;31m"),     // RED
+        GREEN_BOLD("\033[1;32m"),   // GREEN
+        YELLOW_BOLD("\033[1;33m"),  // YELLOW
+        BLUE_BOLD("\033[1;34m"),    // BLUE
+        MAGENTA_BOLD("\033[1;35m"), // MAGENTA
+        CYAN_BOLD("\033[1;36m"),    // CYAN
+        WHITE_BOLD("\033[1;37m"),   // WHITE
+
+        // Underline
+        BLACK_UNDERLINED("\033[4;30m"),     // BLACK
+        RED_UNDERLINED("\033[4;31m"),       // RED
+        GREEN_UNDERLINED("\033[4;32m"),     // GREEN
+        YELLOW_UNDERLINED("\033[4;33m"),    // YELLOW
+        BLUE_UNDERLINED("\033[4;34m"),      // BLUE
+        MAGENTA_UNDERLINED("\033[4;35m"),   // MAGENTA
+        CYAN_UNDERLINED("\033[4;36m"),      // CYAN
+        WHITE_UNDERLINED("\033[4;37m"),     // WHITE
+
+        // Background
+        BLACK_BACKGROUND("\033[40m"),   // BLACK
+        RED_BACKGROUND("\033[41m"),     // RED
+        GREEN_BACKGROUND("\033[42m"),   // GREEN
+        YELLOW_BACKGROUND("\033[43m"),  // YELLOW
+        BLUE_BACKGROUND("\033[44m"),    // BLUE
+        MAGENTA_BACKGROUND("\033[45m"), // MAGENTA
+        CYAN_BACKGROUND("\033[46m"),    // CYAN
+        WHITE_BACKGROUND("\033[47m"),   // WHITE
+
+        // High Intensity
+        BLACK_BRIGHT("\033[0;90m"),     // BLACK
+        RED_BRIGHT("\033[0;91m"),       // RED
+        GREEN_BRIGHT("\033[0;92m"),     // GREEN
+        YELLOW_BRIGHT("\033[0;93m"),    // YELLOW
+        BLUE_BRIGHT("\033[0;94m"),      // BLUE
+        MAGENTA_BRIGHT("\033[0;95m"),   // MAGENTA
+        CYAN_BRIGHT("\033[0;96m"),      // CYAN
+        WHITE_BRIGHT("\033[0;97m"),     // WHITE
+
+        // Bold High Intensity
+        BLACK_BOLD_BRIGHT("\033[1;90m"),    // BLACK
+        RED_BOLD_BRIGHT("\033[1;91m"),      // RED
+        GREEN_BOLD_BRIGHT("\033[1;92m"),    // GREEN
+        YELLOW_BOLD_BRIGHT("\033[1;93m"),   // YELLOW
+        BLUE_BOLD_BRIGHT("\033[1;94m"),     // BLUE
+        MAGENTA_BOLD_BRIGHT("\033[1;95m"),  // MAGENTA
+        CYAN_BOLD_BRIGHT("\033[1;96m"),     // CYAN
+        WHITE_BOLD_BRIGHT("\033[1;97m"),    // WHITE
+
+        // High Intensity backgrounds
+        BLACK_BACKGROUND_BRIGHT("\033[0;100m"),     // BLACK
+        RED_BACKGROUND_BRIGHT("\033[0;101m"),       // RED
+        GREEN_BACKGROUND_BRIGHT("\033[0;102m"),     // GREEN
+        YELLOW_BACKGROUND_BRIGHT("\033[0;103m"),    // YELLOW
+        BLUE_BACKGROUND_BRIGHT("\033[0;104m"),      // BLUE
+        MAGENTA_BACKGROUND_BRIGHT("\033[0;105m"),   // MAGENTA
+        CYAN_BACKGROUND_BRIGHT("\033[0;106m"),      // CYAN
+        WHITE_BACKGROUND_BRIGHT("\033[0;107m");     // WHITE
+
+        private final String code;
+
+        Color(String code) {
+            this.code = code;
+        }
+
+        @Override
+        public String toString() {
+            return code;
+        }
+    }
+
 
     //    Line formating
     private static void line() {
@@ -50,13 +135,14 @@ public class Calculator {
 
     private static void line(String description) {
         System.out.format("+--------------------------------------------------+%n");
-        System.out.format("|\033[1;34m %-48s \033[0m|%n", description);
+//        System.out.format("|\033[1;34m %-48s \033[0m|%n", description);
+        System.out.format("|%s %-48s %s%n", Color.BLUE, description, Color.RESET);
         System.out.format("+--------------------------------------------------+%n");
     }
 
     private static void lineHeader(String description) {
         System.out.format("+--------------------------------------------------+%n");
-        System.out.format("|\033[0;32m %-48s \033[0m|%n", description);
+        System.out.format("|%s %-48s %s|%n", Color.GREEN, description,Color.RESET);
         System.out.format("+--------------------------------------------------+%n");
     }
 
@@ -75,15 +161,15 @@ public class Calculator {
 
     private static void lineError(String description) {
 //        System.out.format("\033[0;91m\033[47m+--------------------------------------------------+\033[0m%n");
-        System.out.format("\033[0;91m+--------------------------------------------------+%s%n",RESET);
-        System.out.format("\033[0;91m| %-48s |%s%n", description,RESET);
-        System.out.format("\033[0;91m+--------------------------------------------------+\033[0m%n");
+        System.out.format("%s+--------------------------------------------------+%s%n",Color.RED, Color.RESET);
+        System.out.format("\033[0;91m| %-48s |%s%n", description, RESET);
+        System.out.format("%s+--------------------------------------------------+%s%n",Color.RED, Color.RESET);
     }
 
     private static void lineInfo(String description) {
-        System.out.format("\033[0;32m+--------------------------------------------------+\033[0m%n");
-        System.out.format("\033[0;32m| %-48s |\033[0m%n", description);
-        System.out.format("\033[0;32m+--------------------------------------------------+\033[0m%n");
+        System.out.format("%s+--------------------------------------------------+%s%n",Color.GREEN, Color.RESET);
+        System.out.format("\033[0;91m| %-48s |%s%n", description, RESET);
+        System.out.format("%s+--------------------------------------------------+%s%n",Color.GREEN, Color.RESET);
     }
 
     private static String getLanguage() {
@@ -207,16 +293,16 @@ public class Calculator {
 
             switch (choice) {
                 case 1:
-                    operationHandler(operationAddEN, "1",additionOutput());
+                    operationHandler(operationAddEN, "1", additionOutput());
                     break;
                 case 2:
-                    operationHandler(operationMulEN, "2",multiplicationOutput());
+                    operationHandler(operationMulEN, "2", multiplicationOutput());
                     break;
                 case 3:
-                    operationHandler(operationSubEN, "3",subtractionOutput());
+                    operationHandler(operationSubEN, "3", subtractionOutput());
                     break;
                 case 4:
-                    operationHandler(operationDivEN, "4",divisionOutput());
+                    operationHandler(operationDivEN, "4", divisionOutput());
                     break;
                 case 5:
                     if (chosenOperations.isEmpty()) {
@@ -246,7 +332,8 @@ public class Calculator {
     //print outputs from operations
     private static void chosenOutputs() {
         if (language == "EN") {
-            line(outputEN);;
+            line(outputEN);
+            ;
         } else {
             line(outputSK);
         }
