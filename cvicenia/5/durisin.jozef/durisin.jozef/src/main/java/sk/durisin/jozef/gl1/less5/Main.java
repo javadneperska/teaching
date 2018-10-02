4c7dbfe034e5aa431e20df625335872bb47163b7
jozef-durisin-gl <jozef.durisin@globallogic.com>
2018-10-02 08:38:08 +0200

    sdgf

--- cvicenia/5/durisin.jozef/durisin.jozef/src/main/java/sk/durisin/jozef/gl1/less5/Main.java
+++ cvicenia/5/durisin.jozef/durisin.jozef/src/main/java/sk/durisin/jozef/gl1/less5/Main.java
@@ -3,7 +3,7 @@ package sk.durisin.jozef.gl1.less5;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Message from lesson 5.");
+        System.out.println("Message from lesson  5.");
 
     }
 
