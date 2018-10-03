e624754dddb2796e458430dfbae25c21f54ddd2c
Jozef Salanci <jozef.salanci@globallogic.com>
2018-10-03 12:39:38 +0200

    refactor

--- domace_ulohy/5/jozef.salanci/src/main/java/sk/salanci/jozef/gl1/Main.java
+++ domace_ulohy/5/jozef.salanci/src/main/java/sk/salanci/jozef/gl1/Main.java
@@ -1,16 +1,6 @@
 package sk.salanci.jozef.gl1;
 
-
-//Probate program kitty nacita diva externe suborn pri com externe subordinates bude obsahovat maticu risle 4*4
-//
-//        Program sa nasse s spyta pouzivate na nazvy subor  nacita tieto subory a urobi  nasobenie matic a vypise vysledok na orazovku
-//
-//        Podporujuci program toot zadnai bude program story zgeneruje 2 subory
-
-
 public class Main {
-//    public static String firstCSV;
-//    public static String secondCSV;
 
     public static String firstFile;
     public static String secondFile;
@@ -22,15 +12,11 @@ public class Main {
         System.out.println("Sucin dvoch matic 4X4");
         firstFile = FileHandler.fileName("prveho");
         secondFile = FileHandler.fileName("druheho");
-//        System.out.println(FileHandler.getPath(firstFile));
-//        System.out.println(FileHandler.getPath(secondFile));
         FileHandler.parserCsv(FileHandler.getPath(firstFile));
         firstArray=FileHandler.myArray.clone();
         FileHandler.parserCsv(FileHandler.getPath(secondFile));
         secondArray=FileHandler.myArray.clone();
-//        FileHandler.arrayOutput(firstArray);
         System.out.println("==================================");
-//        FileHandler.arrayOutput(secondArray);
         FileHandler.matrixMultiplication(firstArray, secondArray);
         FileHandler.arrayOutput(FileHandler.matrixMultiplication);
     }
