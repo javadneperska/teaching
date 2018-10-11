6b19432131a4349f99859935179db69a0d00d5df
Kristian Moravcik <kristian.moravcik@kestramedical.com>
2018-10-11 08:30:07 +0200

    cv8

---
+++ cvicenia/8/moravcik.kristian/src/main/java/sk/moravcik/kristian/gl1/Main.java
@@ -0,0 +1,15 @@
+package sk.moravcik.kristian.gl1;
+
+public class Main {
+    public static void main(String[] args) {
+        Person kristian = new Person();
+        kristian.setAge(28);
+        kristian.setFirstName("Kristian");
+        kristian.setLastName("Moravcik");
+
+
+    }
+
+
+
+}
