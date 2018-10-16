6283216ad732172bb563e2b9da05b3a71da86eaf
miroslav.katrak <miroslav.katrak@globallogic.com>
2018-10-16 01:57:58 -0500

    9

---
+++ cvicenia/9/katrak.miroslav/src/main/java/sk/katrak/miroslav/gl1/less9/Main.java
@@ -0,0 +1,20 @@
+package sk.katrak.miroslav.gl1.less9;
+
+public class Main {
+
+    public static final String NL = "\n";
+
+    public static void main(String args[]) {
+
+        Student miro = new Student();
+        miro.setFirstName("miro");
+        miro.setLastName("katrak");
+        PersonDate miroDob =  new PersonDate(8, 1, 1982);
+        miro.setDob(miroDob);
+
+
+        System.out.println("miro: " + miro.toString());
+
+    }
+
+}
