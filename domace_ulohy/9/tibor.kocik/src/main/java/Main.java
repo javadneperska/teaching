9d515668e5402e0662b0fd3fd1e2968ac186da8b
tibor.kocik <tibor.kocik@globallogic.com>
2018-10-18 08:41:03 +0200

    du9

--- domace_ulohy/9/tibor.kocik/src/main/java/Main.java
+++ domace_ulohy/9/tibor.kocik/src/main/java/Main.java
@@ -1,27 +1,68 @@
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Main {
     //DU 9 - Urobte program, ktory z lokalneho file systemu alebo z lubovolnej DB nacita zoznam studentov, kazdy student bude mat
-    //minimalne 3 predmety, 2 volitelne 1 volitelny, s tym že, predmet bude mať udavane info ako:
-    // vyučujúci, počet hodin do tyzdna, či konči skuškou (true false ) a návaznosť(iba jednu)
+    //minimalne 3 predmety, 2 volitelne 1 volitelny, s tym Å¾e, predmet bude maÅ¥ udavane info ako:
+    // vyuÄujÃºci, poÄet hodin do tyzdna, Äi konÄi skuÅ¡kou (true false ) a nÃ¡vaznosÅ¥(iba jednu)
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
         Class.forName("org.sqlite.JDBC");
         Connection conn = DriverManager.getConnection("jdbc:sqlite:STUDENTS.db");
-        Statement stat = conn.createStatement();
-        //ResultSet rs = stat.executeQuery("select * from tabulka_faktov");
-        ResultSet rs = stat.executeQuery("SELECT tabulka_faktov.MENO, tabulka_faktov.PREDMET1, tabulka_faktov.PREDMET2 FROM tabulka_faktov ");
-        while (rs.next()) {
-            System.out.println(rs.getString("MENO"));
-            System.out.println("predmet 1 = " + rs.getString("PREDMET1"));
-            System.out.println("predmet 2 = " + rs.getString("PREDMET2"));
-            //System.out.println("volitelny = " + rs.getString("VOLITELNY_PREDMET"));
-        }
-        rs.close();
+        ArrayList<String> person = processDBPerson(conn);
+        ArrayList<Predmet> predmet = processDBPredmet(conn);
+        ArrayList<VolPredmet> volPredmet = processDBVolPredmet(conn);
         conn.close();
+        Person[] students = new Person[person.size()];
+        Random rnd = new Random();
+
+        for(int i=0;i<person.size();i++)
+        {
+            students[i] = new Person(person.get(i),predmet.get(i), predmet.get(i),volPredmet.get(rnd.nextInt((2-0)+1)+0));
+            System.out.println(students[i]);
+        }
+
     }
 
 
+    public static ArrayList<String> processDBPerson (Connection conn) throws SQLException {
+        ArrayList<String> res = new ArrayList<String>();
+        Statement stat = conn.createStatement();
+        ResultSet rs = stat.executeQuery("select * from " + "studenti");
 
+        while (rs.next()) {
+            res.add(rs.getString("MENO"));
+        }
+
+        rs.close();
+        return res;
+    }
+
+    public static ArrayList<Predmet> processDBPredmet (Connection conn) throws SQLException {
+        ArrayList<Predmet> res = new ArrayList<Predmet>();
+        Statement stat = conn.createStatement();
+        ResultSet rs = stat.executeQuery("select * from " + "predmety");
+
+        while (rs.next()) {
+            res.add(new Predmet(rs.getString("PREDMET"), rs.getString("VYUCUJUCI"), Integer.parseInt(rs.getString("POCET_HODIN")), Integer.parseInt(rs.getString("SKUSKA"))));
+        }
+
+        rs.close();
+        return res;
+    }
+
+    public static ArrayList<VolPredmet> processDBVolPredmet (Connection conn) throws SQLException {
+        ArrayList<VolPredmet> res = new ArrayList<VolPredmet>();
+        Statement stat = conn.createStatement();
+        ResultSet rs = stat.executeQuery("select * from " + "volitelne_predmety");
+
+        while (rs.next()) {
+            res.add(new VolPredmet(rs.getString("PREDMET"), rs.getString("VYUCUJUCI"), Integer.parseInt(rs.getString("POCET_HODIN")), Integer.parseInt(rs.getString("SKUSKA"))));
+        }
+
+        rs.close();
+        return res;
+    }
 
 }
--- domace_ulohy/9/tibor.kocik/src/main/java/Person.java
+++ domace_ulohy/9/tibor.kocik/src/main/java/Person.java
@@ -1,16 +1,25 @@
 public class Person {
     private static String meno;
-    private static String predmet1;
-    private static String predmet2;
-    private static String volPredmet;
+    private static Predmet predmet1;
+    private static Predmet predmet2;
+    private static VolPredmet volPredmet;
 
-    public Person(String meno, String predmet1, String predmet2, String volPredmet) {
+    public Person(String meno, Predmet predmet1, Predmet predmet2, VolPredmet volPredmet) {
         this.meno = meno;
         this.predmet1 = predmet1;
         this.predmet2 = predmet2;
         this.volPredmet = volPredmet;
     }
 
+    @Override
+    public String toString() {
+        return "Person{" +
+                "meno='" + meno + '\'' +
+                ", predmet1='" + predmet1.toString() + '\'' +
+                ", predmet2='" + predmet2.toString() + '\'' +
+                ", volPredmet='" + volPredmet.toString() +
+                '}';
+    }
 
     public static String getMeno() {
         return meno;
@@ -20,27 +29,28 @@ public class Person {
         Person.meno = meno;
     }
 
-    public static String getPredmet1() {
+    public static Predmet getPredmet1() {
         return predmet1;
     }
 
-    public static void setPredmet1(String predmet1) {
+    public static void setPredmet1(Predmet predmet1) {
         Person.predmet1 = predmet1;
     }
 
-    public static String getPredmet2() {
+    public static Predmet getPredmet2() {
         return predmet2;
     }
 
-    public static void setPredmet2(String predmet2) {
+    public static void setPredmet2(Predmet predmet2) {
         Person.predmet2 = predmet2;
     }
 
-    public static String getVolPredmet() {
+    public static VolPredmet getVolPredmet() {
         return volPredmet;
     }
 
-    public static void setVolPredmet(String volPredmet) {
+    public static void setVolPredmet(VolPredmet volPredmet) {
         Person.volPredmet = volPredmet;
     }
+
 }
